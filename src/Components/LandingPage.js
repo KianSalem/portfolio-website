df87bfbdd3d0d06fdd3d79a45e9af19191b18389
KianSalem <salemkian1@gmail.com>
2024-11-29 04:32:59 -0600

    Commit Commit 3

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div>
       <h1>Kian Salem</h1>
-      <img src="/path-to-your-photo.jpg" alt="Kian Salem" />
+      <img src="/Headshot1.jpeg" alt="Kian Salem" />
       <p>
         AI/ML Enthusiast, Data Analyst, and Product Manager with experience from
         data reporting journalism classes and a year at Scale AI.
