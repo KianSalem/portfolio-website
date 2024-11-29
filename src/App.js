adc72de06a09ef2f0d24c19b53172bbad3a7fc9d
KianSalem <salemkian1@gmail.com>
2024-11-29 05:44:19 -0600

    Maybefixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeContext } from './ThemeContext';
-import LandingPage from './components/LandingPage';
-import Projects from './components/Projects';
-import About from './components/About';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage.js';
+import Projects from './components/Projects.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Navbar from './components/Navbar.js';
 
 function App() {
   const { toggleTheme } = useContext(ThemeContext);
